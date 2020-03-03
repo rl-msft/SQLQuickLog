d695ed56fefd5302df341cfac5ebf8c17ef19397
RL_MSFT <20459971+rl-msft@users.noreply.github.com>
2020-03-03 23:03:32 +0400

    Update SQLQuickLogCheckUpdates.ps1

--- Scripts/SQLQuickLogCheckUpdates.ps1
+++ Scripts/SQLQuickLogCheckUpdates.ps1
@@ -1,8 +1,8 @@
 #Edit: Feb 25 2019
 #v2.0
 
-powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rl-msft/sqlerrorloghelper/master/uninstall.reg' -OutFile $env:temp\uninstall.reg"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rl-msft/SQLQuickLog/master/installation/uninstall.reg' -OutFile $env:temp\uninstall.reg"
 regedit.exe /s "%temp%\uninstall.reg"
 
-powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rl-msft/sqlerrorloghelper/master/setup.reg' -OutFile $env:temp\setup.reg"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rl-msft/SQLQuickLog/master/installation/setup.reg' -OutFile $env:temp\setup.reg"
 regedit.exe /s "%temp%\setup.reg"
